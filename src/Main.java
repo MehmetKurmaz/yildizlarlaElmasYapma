c0e345c49ba558519a18d853d54aeecbd13c26ac
MehmetKurmaz <111184509+MehmetKurmaz@users.noreply.github.com>
2022-10-31 00:08:19 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
             }
             System.out.println(" ");
         }
-        for (int i=0;i<=n;i++) {
+        for (int i=1;i<=n;i++) {
             for (int a= n; a > (n - i); a--) {
                 System.out.print(" ");
             }
